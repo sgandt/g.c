757dc512ae448cc64c3bd3e3b801a40b0ff8a58a
sgandt <35653041+sgandt@users.noreply.github.com>
2018-02-01 16:52:51 +0530

    Create gppp.c

---
+++ gppp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int  i,sum,a,b;
+printf("enter the value");
+scanf("%d",&a);
+for(i=0;i<1;i++)
+{
+    sum=sum+i;
+}
+printf("\n,%d",sum);
+getch();
+}
